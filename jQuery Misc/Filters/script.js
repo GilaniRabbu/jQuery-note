a74f8c40b61c1ada41e97f7a5ba6f04e8c12bf94
Toufiq Gilani <113186897+Toufiq-Gilani@users.noreply.github.com>
2023-08-17 23:43:27 +0600

    Filters

---
+++ jQuery Misc/Filters/script.js
@@ -0,0 +1,14 @@
+// jQuery Misc
+
+// jQuery Filters
+
+// Use jQuery to filter / search for specific elements.
+
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
